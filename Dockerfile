103bc18170cd0aea0a40da652d241888a22ba7d8
unknown <mecaluxivansantobuono@gmail.com>
2020-09-06 20:02:51 -0300

    initial configs and requeriments

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:12.18.2
+
+LABEL maintainer="Santobuono Iván Leonel <mecaluxivansantobuono@gmail.com>"
+LABEL version="1.0.0"
+
+ENV NODE_PORT 5001
+ENV INSTALL_PATH /app
+
+RUN mkdir -p $INSTALL_PATH
+WORKDIR $INSTALL_PATH
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE $NODE_PORT
+
+CMD ["npm","start"]
---
+++ src/index.js
@@ -0,0 +1,45 @@
+'use strict';
+/* The dotenv is a zero-dependency module that loads environment variables from a .env file into process.env . */
+const env = require('dotenv').config();
+/* apirest framework. */
+const express = require('express');
+/* Morgan is basically a logger, on any requests being made,it generates logs automatically. */
+const morgan = require('morgan');
+/* CORS is a nodejs package for providing a Connect/Express middleware that can be used to enable CORS with various options. */
+const cors = require('cors');
+
+//Settings
+/* setting port config */
+const port = process.env.PORT || process.env.NODE_PORT || 8080;
+/* init express app */
+const app = express();
+/* ip configs for req */
+app.set('trust proxy', true);
+
+//Middlewares
+/* config morgan */
+app.use(morgan('dev'));
+/* parse application/json, basically parse incoming Request Object as a JSON Object  */
+app.use(express.json());
+/* parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays */
+app.use(express.urlencoded({extended:true}));
+/* config cors */
+app.use(cors());
+
+//Routes files
+
+
+//Routes Urls
+
+
+//Test Conection
+app.get('/', function (req, res) { res.send('Hello Apirest Template')});
+
+//Server
+try{
+    module.exports = app.listen(port, function () {
+        console.log(`application up and running on port: ${port}`);
+    });
+}catch (e) {
+    console.log(e.message);
+}
